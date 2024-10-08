a70185c782427abb2283694bd244564a999831ec
rc4317809 <rc4317809@gmail.com>
2024-10-08 14:51:48 +0800

    fixed some minor stylings

--- components/CustomDatePicker.tsx
+++ components/CustomDatePicker.tsx
@@ -57,7 +57,7 @@ const CustomCalendarPicker: React.FC<CustomCalendarPickerProps> = ({
     return (
         <View>
             <TouchableOpacity onPress={() => setModalVisible(true)}>
-                <Text className='text-xl text-[#15A86D] px-4 border border-gray-500 rounded-md'>{label}</Text>
+                <Text className='text-xl text-slate-500 px-4 border border-gray-500 rounded-md font-plight'>{label}</Text>
             </TouchableOpacity>
 
             <Modal
